304d343d9ee3940806bc419e0b3363e76fa060a1
Rad Batnag <radamanthus@gmail.com>
2015-08-14 15:02:34 +0800

    Custom chef for rotating daemon.log

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -3,16 +3,17 @@
 # Recipe:: default
 #
 
-remote_file "/etc/logrotate.d/nginx" do
-  source "nginx.logrotate"
+remote_file "/etc/logrotate.d/daemon" do
+  source "daemon.logrotate"
   owner "root"
   group "root"
   mode "0644"
   backup 0
 end
 
-cron "logrotate -f /etc/logrotate.d/nginx" do
+cron "logrotate -f /etc/logrotate.d/daemon" do
+  hour '0'
   minute  '0'
-  command "logrotate -f /etc/logrotate.d/nginx"
+  command "logrotate -f /etc/logrotate.d/daemon"
   user "root"
 end
