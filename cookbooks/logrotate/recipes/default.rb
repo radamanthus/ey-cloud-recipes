8cc574ad0609247f4570e021a2f2f4ec07866573
Rad Batnag <radamanthus@gmail.com>
2015-08-17 11:45:33 +0800

    Hourly logrotate for Nginx and application logs

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 
+# logrotate config Nginx logs
 remote_file "/etc/logrotate.d/nginx" do
   source "nginx.logrotate"
   owner "root"
@@ -11,8 +12,25 @@ remote_file "/etc/logrotate.d/nginx" do
   backup 0
 end
 
+# hourly cron entry for nginx logrotate
 cron "logrotate -f /etc/logrotate.d/nginx" do
   minute  '0'
   command "logrotate -f /etc/logrotate.d/nginx"
+  user "root"
+end
+
+# logrotate config for application logs
+remote_file "/etc/logrotate.d/application-logs" do
+  source "application-logs.logrotate"
+  owner "root"
+  group "root"
+  mode "0644"
+  backup 0
+end
+
+# hourly cron entry for application-logs logrotate
+cron "logrotate -f /etc/logrotate.d/application-logs" do
+  minute  '0'
+  command "logrotate -f /etc/logrotate.d/application-logs"
   user "root"
 end
