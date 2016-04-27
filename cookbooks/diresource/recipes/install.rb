5f7d5b31d8964efe96dedac9a6804f2b8ee34a18
Radamanthus Batnag <radamanthus@gmail.com>
2016-04-27 09:53:25 +0800

    Configure diresource to start on boot

--- cookbooks/diresource/recipes/install.rb
+++ cookbooks/diresource/recipes/install.rb
@@ -8,6 +8,7 @@ cookbook_file "/usr/local/bin/diresource" do
   action :create
 end
 
+# Create the nginx location block
 execute "reload-nginx" do
   action :nothing
   command "/etc/init.d/nginx reload"
@@ -25,4 +26,23 @@ template "/etc/nginx/servers/#{application}/additional_location_blocks.customer"
   notifies :run, resources(:execute => "reload-nginx")
 end
 
+# Configure diresource to start on boot
+template "/etc/init.d/diresource" do
+  owner "root"
+  group "root"
+  mode 0755
+  source "diresource.erb"
+  variables({
+    :port => node[:diresource][:port]
+  })
+end
 
+# This does not work on Gentoo 12.11. Maybe https://docs.chef.io/resource_service.html is not yet supported in Chef 10.6?
+# Consider revisiting this in Gentoo 2016 stack which uses Chef 12
+#service 'diresource' do
+#  action :start
+#end
+
+execute "diresource_start_on_boot" do
+  command "rc-update add diresource default"
+end
